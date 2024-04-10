d49de1db52c84d0e38a60d6d8afbecce2f4a9ae4
Tim Mensinger <mensingertim@gmail.com>
2024-04-10 10:59:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="optimagic",
+    version='0.0.1-alpha',
+    description="The optimization part of the estimagic package",
+    long_description="The optimization part of the estimagic package",
+    author="Janos Gabler, Tim Mensinger",
+    author_email="janos.gabler@gmail.com, mensingertim@gmail.com",
+    url="https://github.com/timmens/optimagic",
+    packages=[],
+)
